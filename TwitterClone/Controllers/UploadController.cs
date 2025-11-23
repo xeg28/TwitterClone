a1992bae8b339afe7f426f96a2c21b4ed88930db
xeg28 <eg2895@gmail.com>
2025-11-23 12:07:31 -0800

    banner upload

--- TwitterClone/Controllers/UploadController.cs
+++ TwitterClone/Controllers/UploadController.cs
@@ -1,6 +1,5 @@
 ﻿using CloudinaryDotNet;
 using CloudinaryDotNet.Actions;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using TwitterClone.Data;
 using TwitterClone.Services;
@@ -19,14 +18,13 @@ namespace TwitterClone.Controllers
             _context = context;
         }
 
-        [HttpPost("image/profile")]
-        public async Task<IActionResult> UploadImage(IFormFile file)
+        private async Task<IActionResult> UploadImage(string folder, IFormFile file) 
         {
             Request.Cookies.TryGetValue("userId", out var userIdString);
             if (userIdString == null)
                 return Unauthorized(new { status = 401, message = "You are unauthorized to edit this resource" });
             var user = await _context.Users.FindAsync(int.Parse(userIdString));
-            
+
             if (user == null) return NotFound();
             if (file == null || file.Length == 0)
                 return BadRequest("No file uploaded.");
@@ -34,35 +32,45 @@ namespace TwitterClone.Controllers
             var uploadParams = new ImageUploadParams
             {
                 File = new FileDescription(file.FileName, file.OpenReadStream()),
-                Folder = "my_app/images/profile"
+                Folder = "my_app/images/"+folder,
+                PublicId = $"user_{folder}_{user.Id}",
+                Overwrite = true,
+                Invalidate = true
             };
 
             var uploadResult = await _cloudinary.UploadAsync(uploadParams);
             var url = uploadResult.SecureUrl.ToString();
 
-            if(user.ProfilePicUrl != null)
-            {
-                Uri uri = new Uri(user.ProfilePicUrl);
-                string path = uri.LocalPath;
-                string filename = System.IO.Path.GetFileNameWithoutExtension(path);
-                var res = await DeleteImage(filename ?? "");
-                if(!res)
-                    return StatusCode(500, new { message="Server error while uploading, try again." });
-            }
-   
+
             Uri newUri = new Uri(url);
 
-        
-            string filePath = newUri.LocalPath;
-     
-            string pathWithNewExtension = Path.ChangeExtension(filePath, ".webp");
-       
-            string newUrl = newUri.Scheme + "://" + newUri.Authority + pathWithNewExtension + newUri.Query;
 
-            user.ProfilePicUrl = newUrl;
+            string filePath = newUri.LocalPath;
+
+            string pathWithNewExtension = Path.ChangeExtension(filePath, ".webp");
+
+            string newUrl = newUri.Scheme + "://" + newUri.Authority + pathWithNewExtension + newUri.Query;
+               
+            if (folder == "banner")
+                user.BannerPicUrl = newUrl;
+            else
+                user.ProfilePicUrl = newUrl;
             await _context.SaveChangesAsync();
-            return Ok(new { url });
+            return Ok(new { url = newUrl });
         }
+
+        [HttpPost("image/profile")]
+        public async Task<IActionResult> UploadProfileImage(IFormFile file)
+        {
+            return await UploadImage("profile", file);
+        }
+
+        [HttpPost("image/banner")]
+        public async Task<IActionResult> UploadProfileBanner(IFormFile file)
+        {
+            return await UploadImage("banner", file);
+        }
+
 
         private async Task<bool> DeleteImage(string publicId)
         {
